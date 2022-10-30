5310880702833d1bfad849fae9c7766eb1c8f12a
PavelGodes <pgodes1@gmail.com>
2022-10-30 18:57:34 +0200

    HW#11 Version 2

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 console.log("Hello SumDigits.js");
 function sumDigits(number) {
-    if(isNaN(number)){
-        return'Error';
+number = Math.abs(number);
+number = Math.trunc(number);
+let sum = 0;
+do {
+    
+    let digit = number % 10;
+    number = (number - digit) / 10;
+    sum += digit;
 
-    }
-    if(number<0){
-        number=-number;
-    }
-    let sum=0;
-    while(!(number == 0)) {
-        sum += number %10;
-        number = Math.floor(number/10);
 
-    }
+}while(number != 0);
+
+
+
+
+    // if(isNaN(number)){
+    //     return'Error';
+
+    // }
+    // if(number<0){
+    //     number=-number;
+    // }
+    // let sum=0;
+    // while(!(number == 0)) {
+    //     sum += number %10;
+    //     number = Math.floor(number/10);
+
+    // }
     return sum;
 }
+
 console.log('sum1=',sumDigits(123));
 console.log('sum2=',sumDigits(-623));
